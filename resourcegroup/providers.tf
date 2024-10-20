3dd9020fa641c2cc9cd099e82c44735ea4b8f722
Deva <dvrharsha@gmail.com>
2024-10-20 13:33:43 -0400

    updated rg

--- resourcegroup/providers.tf
+++ resourcegroup/providers.tf
@@ -8,5 +8,15 @@ terraform {
 }
 
 provider "azurerm" {
+  resource_provider_registrations = "none"
   features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tf-str-29"
+    storage_account_name = "tfstrbck29"
+    container_name       = "terraform-state"
+    key                  = "rg.terraform.tfstate"
+  }
 }
